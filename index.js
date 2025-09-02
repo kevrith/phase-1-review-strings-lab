dab80ddf6628326a44b0ff7cb96350217780d1de
KELVIN <kevrith@gmail.com>
2025-09-02 15:41:11 +0300

    Compelete Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "KELVIN";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const firstInitial = currentUser.slice(0, 1);
+const shortGreeting = "Welcome, " + firstInitial + "!"
+console.log(currentUser);
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
+console.log(shortGreeting);
